b80161e314b7ee7044ed93daa840c6b252f212b8
gustavo <gustavoadrianvillalba@gmail.com>
2025-04-27 20:10:38 +0200

    Build exitoso - Listo para producción

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat, Roboto } from "next/font/google";
 import "./globals.css";
+export const dynamic = "force-static";
 
 const geistMontserrat = Montserrat({
   variable: "--font-montserrat",
